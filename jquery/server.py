359ee5cd5d194e143531fadf9ef7db7c9a22fb96
Adam Kimball <Bahamut2k456@gmail.com>
2014-10-27 23:06:02 -0400

    Front-end now reads json from server and constructs a table

--- jquery/server.py
+++ jquery/server.py
@@ -4,30 +4,31 @@ app = Flask(__name__, static_url_path='/static')
 @app.route("/search")
 def do_stub_search():
     q = request.args.get("q", "nothing")
+    print(q)
     return """
     [
     {
-        name: "Everyone Poops",
-        description: "An enthralling children's story",
-        isbn: "QAAAA-BBBB-CCCC",
-        cover_image: "data:image/gif;base64,R0lGODlhEAAQAMQAAORHHOVSKudfOulrSOp3WOyDZu6QdvCchPGolfO0o/XBs/fNwfjZ0frl3/zy7////wAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACH5BAkAABAALAAAAAAQABAAAAVVICSOZGlCQAosJ6mu7fiyZeKqNKToQGDsM8hBADgUXoGAiqhSvp5QAnQKGIgUhwFUYLCVDFCrKUE1lBavAViFIDlTImbKC5Gm2hB0SlBCBMQiB0UjIQA7",
-        available: 2,
-        quantity: 2,
-        active_date: "2012-04-23T18:25:43.511Z",
-        permission: "FreeToCheckOut"
+        "name": "The Cat and the Gat",
+        "description": "An enthralling children's story",
+        "isbn": "QAAAA-BBBB-CCCC",
+        "cover_image": "data:image/gif;base64,R0lGODlhEAAQAMQAAORHHOVSKudfOulrSOp3WOyDZu6QdvCchPGolfO0o/XBs/fNwfjZ0frl3/zy7////wAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACH5BAkAABAALAAAAAAQABAAAAVVICSOZGlCQAosJ6mu7fiyZeKqNKToQGDsM8hBADgUXoGAiqhSvp5QAnQKGIgUhwFUYLCVDFCrKUE1lBavAViFIDlTImbKC5Gm2hB0SlBCBMQiB0UjIQA7",
+        "available": 2,
+        "quantity": 2,
+        "active_date": "2012-04-23T18:25:43.511Z",
+        "permission": "FreeToCheckOut"
     },
     {
-        name: "Q",
-        description: "%s",
-        isbn: "QAAAA-BBBB-CCCC",
-        cover_image: "data:image/gif;base64,R0lGODlhEAAQAMQAAORHHOVSKudfOulrSOp3WOyDZu6QdvCchPGolfO0o/XBs/fNwfjZ0frl3/zy7////wAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACH5BAkAABAALAAAAAAQABAAAAVVICSOZGlCQAosJ6mu7fiyZeKqNKToQGDsM8hBADgUXoGAiqhSvp5QAnQKGIgUhwFUYLCVDFCrKUE1lBavAViFIDlTImbKC5Gm2hB0SlBCBMQiB0UjIQA7",
-        available: 2,
-        quantity: 2,
-        active_date: "2012-04-23T18:25:43.511Z",
-        permission: "FreeToCheckOut"
+        "name": "Everyone Poops",
+        "description": "Well it's true",
+        "isbn": "QAAAA-BBBB-CCCC",
+        "cover_image": "data:image/gif;base64,R0lGODlhEAAQAMQAAORHHOVSKudfOulrSOp3WOyDZu6QdvCchPGolfO0o/XBs/fNwfjZ0frl3/zy7////wAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACH5BAkAABAALAAAAAAQABAAAAVVICSOZGlCQAosJ6mu7fiyZeKqNKToQGDsM8hBADgUXoGAiqhSvp5QAnQKGIgUhwFUYLCVDFCrKUE1lBavAViFIDlTImbKC5Gm2hB0SlBCBMQiB0UjIQA7",
+        "available": 2,
+        "quantity": 2,
+        "active_date": "2012-04-23T18:25:43.511Z",
+        "permission": "FreeToCheckOut"
     }
     ]
-    """ % q
+    """ 
 
 @app.route("/index.html")
 def index():
---
+++ jquery/static/js/alexandria.js
@@ -0,0 +1,34 @@
+function drawTable(data) {
+	data=JSON.parse(data);
+    for (var i = 0; i < data.length; i++) {
+        drawRow(data[i]);
+    }
+}
+
+function drawRow(rowData) {
+    var row = $("<tr />")
+    $("#bookTable").append(row);
+    row.append($("<td><img src=\"" + rowData.cover_image + "\"/></td>"));
+    row.append($("<td>" + rowData.name + "</td>"));
+    row.append($("<td>" + rowData.description + "</td>"));
+    row.append($("<td>" + rowData.isbn + "</td>"));
+    row.append($("<td>" + rowData.available + "</td>"));
+    row.append($("<td>" + rowData.quantity + "</td>"));
+    row.append($("<td>" + rowData.active_date + "</td>"));
+    row.append($("<td>" + rowData.permission + "</td>"));
+}
+
+$( "#search-button" ).click( function(){
+ 	console.log("This is a console printing test!");
+ 	$.get("/search?q=" + $("#search-field").val())
+	.done(function(data){
+		$("body").empty();
+		$("body").append($("<table class=\"table table-striped table-bordered\" id=\"bookTable\"></table>"))
+		drawTable(data);	
+  	})
+
+  	.fail(function(data){
+		alert("Connection to API Server Failed!");
+	});
+	return false;
+});
