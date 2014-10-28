3289b5a44c16451ebbb91cf80c111bd1a664bbd5
Corey Richardson <corey@octayn.net>
2014-10-27 21:40:25 -0400

    Shuffle some stuff around

--- jquery/server.py
+++ jquery/server.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 
 @app.route("/search")
 def do_stub_search():
@@ -28,6 +28,11 @@ def do_stub_search():
     }
     ]
     """ % q
+
+@app.route("/index.html")
+def index():
+    return open("index.html").read()
+    #app.send_static_file("index.html")
 
 if __name__ == "__main__":
     app.run(debug = True)
