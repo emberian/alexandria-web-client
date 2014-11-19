8ce28d1c559e73f971279712f506963d55e94f3a
Corey Richardson <corey@octayn.net>
2014-11-19 14:54:31 -0500

    Reformat, use versioned API endpoint

--- jquery/static/js/alexandria.js
+++ jquery/static/js/alexandria.js
@@ -1,5 +1,5 @@
 function drawTable(data) {
-	//data=JSON.parse(data);
+    //data=JSON.parse(data);
     for (var i = 0; i < data.data.length; i++) {
         console.log("row " + data.data[i]);
         drawRow(data.data[i]);
@@ -8,7 +8,7 @@ function drawTable(data) {
 
 function drawRow(rowData) {
     var row = $("<tr />")
-    $("#bookTable").append(row);
+        $("#bookTable").append(row);
     row.append($("<td><img src=\"" + rowData.cover_image + "\"/></td>"));
     row.append($("<td>" + rowData.name + "</td>"));
     row.append($("<td>" + rowData.description + "</td>"));
@@ -20,17 +20,17 @@ function drawRow(rowData) {
 }
 
 $( "#search-form" ).submit( function(){
- 	console.log("This is a console printing test!");
- 	//$.get("/api/search?q=" + $("#search-field").val())
-    $.get("/api/book")
-	.done(function(data){
-		$("body").empty();
-		$("body").append($("<table class=\"table table-striped table-bordered\" id=\"bookTable\"></table>"))
-		drawTable(data);
-  	})
+    console.log("This is a console printing test!");
+    //$.get("/api/v1/search?q=" + $("#search-field").val())
+    $.get("/api/v1/book")
+        .done(function(data){
+            $("body").empty();
+            $("body").append($("<table class=\"table table-striped table-bordered\" id=\"bookTable\"></table>"))
+                drawTable(data);
+        })
 
-  	.fail(function(data){
-		alert("Connection to API Server Failed!");
-	});
-	return false;
+    .fail(function(data){
+        alert("Connection to API Server Failed!");
+    });
+    return false;
 });
